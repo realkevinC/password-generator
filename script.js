8a76b162f616338b2ad54f1bdebfcfdd3ee3bcf5
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-10-03 15:42:29 -0400

    update needs fixing

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Array set
-var lowercaseLetter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var uppercaseLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var number = ['1','2','3','4','5','6','7','8','9','0'];
-var specialCharacter = '!@#$%^&*./,<>?;:[]{}|=-+_)(`~'.split('');
-var newPassword = lowercaseLetter.concat(uppercaseLetter,number,specialCharacter)
-// create random charater from the set
 
 // Write password to the #password input
 function writePassword() {
@@ -24,23 +17,32 @@ generateBtn.addEventListener("click", writePassword);
 // Start function.
 // Ask for what criteria to include.
 
-// Ask for length of password that is  more than 8 and  less than 128 character.
+
 function generatePassword() {
-  
-  begin = window.confirm ('Do you want to create a new password?');
+
+// Array set
+
+var lowercaseLetter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercaseLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var number = ['1','2','3','4','5','6','7','8','9','0'];
+var specialCharacter = '!@#$%^&*./,<>?;:[]{}|=-+_)(`~'.split('');
+// var newPassword = lowercaseLetter.concat(uppercaseLetter,number,specialCharacter)
+
+begin = window.confirm ('Do you want to create a new password?');
   if (begin){
     passwordLength();
     requirement();
     // getRandomPassword();
-
+    
     // var generatedPassword = getRandomPassword();
     // return generatedPassword;
     
-
+    
   } else {
     window.alert("Ok. Maybe next time.");
   };
   
+  // Ask for length of password that is  more than 8 and  less than 128 character.
   function passwordLength() {
     lengthOfPassword = prompt("What is the length of your password? Must be between 8 and 128 character");
     lengthOfPassword = Number(lengthOfPassword);
@@ -135,7 +137,7 @@ function generatePassword() {
       console.log(password)
       return password;
     }
-    getRandomPassword()
+    // getRandomPassword()
   };
     
 }
